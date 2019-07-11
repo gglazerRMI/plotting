027e6aa91ed1f28e1a871dcbdd904a878b0401c0
Grant Glazer <gglazer@rmi.org>
2019-07-10 21:25:33 -0400

    map colors plants by type

--- maps.py
+++ maps.py
@@ -17,19 +17,65 @@ df1 = df_dsm
 
 dfp = df1.loc[(df1['Scenario'] == scenario), :]
 count = dfp['Data\nCaseInfo\nLatitude'].isnull().sum()
-
-fig = go.Figure()
-map = go.Scattergeo(
-        locationmode = 'USA-states',
-        lat = dfp['Data\nCaseInfo\nLatitude'],
-        lon = dfp['Data\nCaseInfo\nLongitude'],
-        text = dfp['Case'],
-        marker = go.scattergeo.Marker(
-            size = dfp['Data\nCaseInfo\nCapacity (MW)']/100,
-            color = 'rgb(10,20,30)'
+types = list(set(dfp['Data\nCaseInfo\nType'].unique()))
+# print(dfp.loc[(dfp['Data\nCaseInfo\nType'] == types[1]), 'Data\nCaseInfo\nLatitude'])
+colors = ['rgb(0,255,255)', 'rgb(0,0,128)']
+scale = 100
+# fig = go.Figure()
+plants = []
+for i in range(len(types)):
+    dft = dfp.loc[(dfp['Data\nCaseInfo\nType'] == types[i])]
+    type = go.Scattergeo(
+            locationmode='USA-states',
+            lat=dft['Data\nCaseInfo\nLatitude'],
+            lon=dft['Data\nCaseInfo\nLongitude'],
+            # text=dfp['Case'],
+            marker=go.scattergeo.Marker(
+                size=dfp['Data\nCaseInfo\nCapacity (MW)']/scale,
+                color=colors[i],
+                line=go.scattergeo.marker.Line(
+                    width=0.5, color='rgb(25,25,112)'
+                ),
+            # name=types[i]
+            )
         )
-    )
+    # fig.add_trace(type)
+    plants.append(type)
 
-fig.add_trace(map)
-fig['layout'].update(geo=go.layout.Geo(scope='usa', projection=go.layout.geo.Projection(type='albers usa')))
+# fig.add_trace(plants)
+layout = go.Layout(
+    title=go.layout.Title(
+        text='Announced Planned Gas Plants as of June 2019'
+    ),
+    showlegend=True,
+    geo=go.layout.Geo(
+        scope='usa',
+        projection=go.layout.geo.Projection(
+            type='albers usa'
+        ),
+        showland=True,
+        landcolor='rgb(217,217,217)',
+        subunitwidth=1,
+        countrywidth=1,
+        subunitcolor="rgb(225,225,225)",
+        countrycolor="rgb(225,225,225)"
+    )
+)
+# fig['layout'].update(title=go.layout.Title(
+#         text='Announced Planned Gas Plants as of June 2019'
+#     ),
+#     showlegend=True,
+#     geo=go.layout.Geo(
+#         scope='usa',
+#         projection=go.layout.geo.Projection(
+#             type='albers usa'
+#         ),
+#         showland=True,
+#         landcolor='rgb(217,217,217)',
+#         subunitwidth=1,
+#         countrywidth=1,
+#         subunitcolor="rgb(225,225,225)",
+#         countrycolor="rgb(225,225,225)"
+#     ))
+fig = go.Figure(data=plants, layout=layout)
 pio.write_image(fig, fig_path + 'scatter_map.png')
